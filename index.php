e08abc79b9069bc4d37ae51fa7f01f7eb7b3bc87
cookierilla-auf <escoto.edzlrenzo@student.auf.edu.ph>
2026-03-10 19:12:25 +0800

    index routing and vlucas env done
    there is still error

--- index.php
+++ index.php
@@ -1,11 +1,39 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/config/database.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$db = getConnection();
+
+$catModel = new \App\Models\CatModel($db);
+$catController = new \App\Controllers\CatController($catModel);
 
 $router = new \Bramus\Router\Router();
 
-$router->get('/', function() {
-    echo "Welcome to the AUF SIA API Platform";
+$router->mount('/cats', function() use ($router, $catController) {
+
+    $router->get('/', function() use ($catController) {
+        $catController->index();
+    });
+
+    $router->get('/(\d+)', function($id) use ($catController) {
+        $catController->show($id);
+    });
+
+    $router->post('/', function() use ($catController) {
+        $catController->store();
+    });
+
+    $router->put('/(\d+)', function($id) use ($catController) {
+        $catController->update($id);
+    });
+
+    $router->delete('/(\d+)', function($id) use ($catController) {
+        $catController->destroy($id);
+    });
 });
 
 $router->run();
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit620a40e5d29a92b28a9f9bd79083c9d2
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit620a40e5d29a92b28a9f9bd79083c9d2::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
